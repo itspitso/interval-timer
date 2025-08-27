c105145e59a177f5476b188bff1e25be9e8502cb
itspitso <pitsomashilo1@gmail.com>
2025-08-27 09:47:59 +0200

    feat: added all elements to the script

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const workHours = document.querySelector(".work-hours");
+const workMinutes = document.querySelector(".work-minutes");
+const workSeconds = document.querySelector(".work-seconds");
+
+const restHours = document.querySelector(".rest-hours");
+const restMinutes = document.querySelector(".rest-minutes");
+const restSeconds = document.querySelector("rest-seconds");
+
+const intervals = document.querySelector(".intervals");
+
+const startButton = document.querySelector(".start");
+
+startButton.addEventListener("click", () => {
+    startButton.style.backgroundColor = "red";
+});
